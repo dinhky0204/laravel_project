9b719e12eb1fda197bb441994f3432189bc94ee4
Dinh Ky <dinhky0204@gmail.com>
2017-05-11 13:25:47 +0700

    config email

---
+++ database/migrations/2017_03_28_025128_posts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->string('post_id')->unique();
+            $table->text('value');
+            $table->integer('expiration');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
